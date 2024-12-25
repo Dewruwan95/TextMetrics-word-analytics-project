4cafd760221e6552e95342f90b5fdc60c23da072
Ositha Dewruwan <dewruwanebay@gmail.com>
2024-12-25 20:48:04 +0530

    completed stats section with calculations

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,11 +1,21 @@
+import { useState } from "react";
 import Stats from "./Stats";
 import TextArea from "./TextArea";
 
 export default function Container() {
+  const [text, setText] = useState("");
+
+  const stats = {
+    numberOfWords: text.split(/\s/).filter((word) => word !== "").length,
+    numberOfCharacters: text.length,
+    instagramCharactersLeft: 280 - text.length,
+    facebookCharactersLeft: 2200 - text.length,
+  };
+
   return (
     <main className="container">
-      <TextArea />
-      <Stats />
+      <TextArea text={text} setText={setText} />
+      <Stats stats={stats} />
     </main>
   );
 }
